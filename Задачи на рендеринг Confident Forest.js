ddf7f40895324a2fc75de5a536a628ec0334d6a7
Alex Vedernikov <100806495+AlexVedernikoff@users.noreply.github.com>
2023-01-27 09:01:15 +0300

    Update Задачи на рендеринг Confident Forest.js

--- Задачи на рендеринг Confident Forest.js
+++ Задачи на рендеринг Confident Forest.js
@@ -11,7 +11,7 @@ export const Test1 = () => {
   );
 };
 
-// Решение: оборачиваем компонент Counter в React.memo
+// Решение: оборачиваем компонент Counter в React.memo()
 
 const Counter = React.memo(({ name, value }) => {
   console.log(`Rendering counter ${name}`);
@@ -23,3 +23,27 @@ const Counter = React.memo(({ name, value }) => {
 });
 
 // Теперь компонент <Counter /> будет рендериться ТОЛЬКО при изменении его props ( ...value={counterA})
+
+/// Задача Test2 ////////////////////////////////////////////////////////////////////////////////////
+// Решение:
+// 1. Оборачиваем компонент <Counter /> в React.memo()
+
+const Counter = React.memo(({ name, value, onClickIncrement }) => {
+  ...
+});
+  
+//   Теперь компонент <Counter /> будет рендериться ТОЛЬКО при изменении его props   {name, value, onClickIncrement} 
+  
+// 2. Оборачиваем функцию, которую мы передаём в качестве props в <Counter />, в useCallback(), чтобы при   
+// перерендеринге родительского компонента не создавалась каждый раз новая функция. 
+  
+  const onClickA = useCallback(
+    () => setCounterA((counterA) => counterA + 1),
+    []
+  );  
+  
+ // Передаём функцию onClickA в качестве props в компонент < Counter />
+  
+<Counter name="A" value={counterA} onClickIncrement={onClickA} />
+  
+ // Теперь <Counter /> будет рендериться заново только при изменении counterA в state родительского компонента. 
